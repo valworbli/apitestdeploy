97c3a471846ca3ac1ce400781622958ce1b0b59f
Valentin Ivanov <jira@ftpdaemon.net>
2019-03-26 14:15:08 +0200

    CodeDeploy VII

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,6 +8,8 @@ MYPWD=${0:ZERO:MYLENGTH}
 
 cd $MYPWD
 
-systemctl restart docker
+# systemctl restart docker
 # for dock in $(docker ps | awk '{print $1}' | grep -v CONTAINER); do docker stop $dock; done
-# su ubuntu -c 'docker-compose -f ../docker-compose.yml -f ../docker-compose.dev.yml up -d'
+cd ../
+rm -fr node_modules
+su ubuntu -c 'docker-compose -f ../docker-compose.yml -f ../docker-compose.dev.yml up -d'
