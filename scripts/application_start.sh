e01b043394935786dac790a46883663abb12981a
Valentin Ivanov <jira@ftpdaemon.net>
2019-03-26 13:21:29 +0200

    CodeDeploy III

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,6 +8,6 @@ MYPWD=${0:ZERO:MYLENGTH}
 
 cd $MYPWD
 
-for dock in $(docker ps | awk '{print $1}' | grep -v CONTAINER); do docker stop $dock; done
-systemctl restart docker
+# systemctl restart docker
+# for dock in $(docker ps | awk '{print $1}' | grep -v CONTAINER); do docker stop $dock; done
 docker-compose -f ../docker-compose.yml -f ../docker-compose.dev.yml up -d
